6e10a140eb9db53acc3b2e874e8bcc6c7ae72fa8
rorusage <rorusage@gmail.com>
2016-11-14 16:52:09 +0800

    Build action destroy

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -35,6 +35,12 @@ class BoardsController < ApplicationController
     end
   end
 
+  def destroy
+    @board = Board.find(params[:id])
+    @board.destroy
+    redirect_to boards_path
+  end
+
   private
 
   def board_params
