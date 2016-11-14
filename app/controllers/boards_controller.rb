10a2e8284440dd0586797164bb62e8f59e580f9d
rorusage <rorusage@gmail.com>
2016-11-14 16:33:14 +0800

    Build action create

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -6,4 +6,20 @@ class BoardsController < ApplicationController
   def new
     @board = Board.new
   end
+
+  def create
+    @board = Board.new(board_params)
+
+    if @board.save
+      redirect_to boards_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:title, :description)
+  end
 end
