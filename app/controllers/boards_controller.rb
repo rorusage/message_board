e242fa42e30966ed65486bc2a3628b5cc0e9011f
rorusage <rorusage@gmail.com>
2016-11-14 16:50:29 +0800

    Build action edit and update

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -21,6 +21,20 @@ class BoardsController < ApplicationController
     @board = Board.find(params[:id])
   end
 
+  def edit
+    @board = Board.find(params[:id])
+  end
+
+  def update
+    @board = Board.find(params[:id])
+
+    if @board.update(board_params)
+      redirect_to boards_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def board_params
